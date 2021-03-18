52bd3c439eede37a58097371c0fa5a580cf05e3c
lerahkp <52873528+lerahkp@users.noreply.github.com>
2021-03-18 12:12:16 +0545

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,9 @@ student_scores = {
   "Draco": 74,
   "Neville": 62,
 }
-# 🚨 Don't change the code above 👆
 
-#TODO-1: Create an empty dictionary called student_grades.
 student_grades={}
 
-#TODO-2: Write your code below to add the grades to student_grades.👇
 for student in student_scores:
   score=student_scores[student]
   if score > 90:
@@ -21,8 +18,7 @@ for student in student_scores:
     student_grades[student]="Acceptable"
   else:
     student_grades[student]="Fail"
-    
-# 🚨 Don't change the code below 👇
+
 print(student_grades)
 
 
